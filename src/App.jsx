08cacf3f19c231a83d3d7392027fe37169dfff17
devenamul <devenamul@gmail.com>
2025-04-09 02:26:59 +0600

    core route complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './features/pages/Home/Home'
+import About from './features/pages/About/About'
+import Service from './features/pages/Service/Service'
+import Blog from './features/pages/Blog/Blog'
+import Portfolio from './features/pages/Portfolio/Portfolio'
+import Contact from './features/pages/Contact/Contact'
 
-import './App.css'
 
 function App() {
-
-
   return (
-    <></>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+              <Route path='/' element={< Home/>}/> 
+              <Route path='/about' element={< About/>}/> 
+              <Route path='/service' element={< Service/>}/> 
+              <Route path='/blog' element={< Blog/>}/> 
+              <Route path='/portfolio' element={< Portfolio/>}/> 
+              <Route path='/contact' element={< Contact/>}/> 
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
